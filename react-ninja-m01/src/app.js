05dec38f34f9cdfb48f5fd56f8c6e4a54ae61383
Tayná Martins Ramos <simples.tayna@gmail.com>
2021-03-29 11:26:29 -0300

    start part 2 - module 1

---
+++ react-ninja-m01/src/app.js
@@ -0,0 +1,18 @@
+'use strict'
+
+import React from 'react'
+import Title from './title'
+
+const App = React.createClass({
+  render: function () {
+    return (
+      <div className="container">
+        <Title name="Tayná" />
+        <label htmlFor="input" data-label="Label">Input</label>
+        <input type="text" id="input" aria-hidden={true} />
+      </div>
+    )
+  }
+})
+
+export default App
---
+++ react-ninja-m01/src/title.js
@@ -0,0 +1,11 @@
+'use strict'
+
+import React from 'react'
+
+const Title = React.createClass({
+  render: function () {
+  return <h1>Olá {this.props.name}!</h1>
+  }
+})
+
+export default Title
