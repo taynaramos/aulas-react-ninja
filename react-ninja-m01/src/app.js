21aa2eb2cfdfdb6447e5a4a73afd10054e5ab405
Tayná Ramos <tayna.ramos@ifood.com.br>
2022-10-08 09:56:48 -0300

    getDefaultProps

--- react-ninja-m01/src/app.js
+++ react-ninja-m01/src/app.js
@@ -6,10 +6,10 @@ import Title from './title'
 const App = React.createClass({
   render: function () {
     return (
-      <div className="container">
-        <Title name="Tayná" />
-        <label htmlFor="input" data-label="Label">Input</label>
-        <input type="text" id="input" aria-hidden={true} />
+      <div className='container'>
+        <Title name='Will' lastname='Smith' />
+        <label htmlFor='input' data-label='Label'>Input</label>
+        <input type='text' id='input' aria-hidden={true} />
       </div>
     )
   }
--- react-ninja-m01/src/title.js
+++ react-ninja-m01/src/title.js
@@ -3,8 +3,14 @@
 import React from 'react'
 
 const Title = React.createClass({
+  getDefaultProps: function () {
+    return {
+      name: 'Desconhecido',
+      lastname: 'Sem sobrenome'
+    }
+  },
   render: function () {
-  return <h1>Olá {this.props.name}!</h1>
+  return <h1>Olá {this.props.name + ' ' + this.props.lastname}!</h1>
   }
 })
 
