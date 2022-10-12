994bf34f883d554907906c97e622dba817c6e8f3
Tayná Ramos <tayna.ramos@ifood.com.br>
2022-10-12 19:32:18 -0300

    utilizando funções puras para criar componentes

--- react-ninja-m01/src/title.js
+++ react-ninja-m01/src/title.js
@@ -2,16 +2,27 @@
 
 import React from 'react'
 
-const Title = React.createClass({
-  getDefaultProps: function () {
-    return {
-      name: 'Desconhecido',
-      lastname: 'Sem sobrenome'
-    }
-  },
-  render: function () {
-  return <h1>Olá {this.props.name + ' ' + this.props.lastname}!</h1>
-  }
-})
+const Title = ({ name, lastname }) => {
+  return (
+    <h1>Olá {`${name} ${lastname}`}!</h1>
+  )
+}
+
+Title.defaultProps = {
+  name: 'Desconhecido',
+  lastname: 'Sem sobrenome'
+}
+
+// const Title = React.createClass({
+//   getDefaultProps: function () {
+//     return {
+//       name: 'Desconhecido',
+//       lastname: 'Sem sobrenome'
+//     }
+//   },
+//   render: function () {
+//   return <h1>Olá {this.props.name + ' ' + this.props.lastname}!</h1>
+//   }
+// })
 
 export default Title
