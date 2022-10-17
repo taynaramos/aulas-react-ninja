ce2209577d9489c837b7cb62abc7abb63ed6fd30
Tayná Ramos <tayna.ramos@ifood.com.br>
2022-10-17 18:14:44 -0300

    introdução a propTypes

--- react-ninja-m01/src/app.js
+++ react-ninja-m01/src/app.js
@@ -1,61 +1,14 @@
 'use strict'
 
 import React, { Component } from 'react'
-import Title from './title'
-import Square from './square'
 import Button from './button'
-import Timer from './timer'
 
 class App extends Component {
-  constructor () {
-    console.log('constructor app')
-    super()
-    this.state = {
-      color: 'green',
-      showTimer: true,
-      time: 0
-    }
-  }
-
-  // executa no lado do frontend e do servidor
-  componentWillMount () {
-    console.log('componentWillMount app')
-  }
-
-  // executa somente no lado do frontend
-  componentDidMount () {
-    console.log('componentDidMount app')
-  }
-
   render () {
-    console.log('render app')
     return (
-      <main>
-        <div className='container'>
-          <Title name='Will' lastname='Smith' />
-          <label htmlFor='input' data-label='Label'>Input</label>
-          <input type='text' id='input' aria-hidden={true} />
-          {['blue', 'red', 'green'].map((square, index) => (
-            <Square key={index} color={square} />
-          ))}
-        </div>
-        <div style={{ marginTop: '20px' }}>
-          <Square color={this.state.color} />
-
-          {['blue', 'red', 'green'].map((color) => (
-            <Button 
-              key={color}
-              handleClick={() => this.setState({ color })}
-            >
-              {color}
-            </Button>
-          ))}
-        </div>
-        <div style={{ marginTop: '20px' }}>
-          <Timer time={this.state.time} />
-          <button onClick={() => this.setState({ time: this.state.time + 10 })}>Change props</button>
-        </div>
-      </main>
+      <div>
+        <Button handleClick={() => console.log('clicou')}>Clique em mim</Button>
+      </div>
     )
   }
 }
--- react-ninja-m01/src/button.js
+++ react-ninja-m01/src/button.js
@@ -1,11 +1,17 @@
 'use strict'
 
 import React from 'react'
+import PropTypes from 'prop-types'
 
 const Button = ({ children, handleClick }) => {
   return (
     <button onClick={handleClick}>{children}</button>
   )
+}
+
+Button.propTypes = {
+  handleClick: PropTypes.func.isRequired,
+  childre: PropTypes.string
 }
 
 export default Button
